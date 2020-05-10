e463319538d5fc65aa55661b599546d0bf568c35
Adrian Vaduva <adrian.vaduva@machteamsoft.ro>
2020-05-10 17:05:45 +0300

    install redux and configure store

---
+++ src/redux/reducers/cart.js
@@ -0,0 +1,21 @@
+const initialState = {
+    products: []
+}
+
+export function cartReducer(state = initialState, action) {
+    switch (action.type) {
+        case 'ADD_TO_CART':
+            const newState = {
+                ...state,
+                products: [
+                    ...state.products,
+                    action.payload.product
+
+                ]
+            }
+            return newState;
+
+        default:
+            return state;
+    }
+}
